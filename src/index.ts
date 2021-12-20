62279350c0001f1a715339266d40fdec6d2969a2
mlzzen <mlzzen@qq.com>
2021-12-21 00:27:39 +0800

    release

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,45 @@
-import type { Plugin, ResolvedConfig } from 'vite'
+import type { PluginOption } from 'vite'
 import fs from 'fs';
 import archiver from 'archiver';
 
 interface UserOptions {
-    format: 'zip' | 'tar',
     source?: string,
     destination?: string
 }
 
-function VitePluginFileManager(userOptions: UserOptions = {
-    format: 'zip',
-}): Plugin {
+export function filemanager(userOptions: UserOptions = {
+    source: './dist',
+    destination: './dist.zip',
+}): PluginOption {
     const { source, destination } = userOptions;
-    const output = fs.createWriteStream(destination || './dist.zip');
-    const archive = archiver('zip', {
-        zlib: { level: 9 }
-    })
+
     return {
         name: 'vite-plugin-file-manager',
-        apply: 'build'
+        apply: 'build',
+        configResolved() {
+        },
+        closeBundle() {
+            console.log('destination', destination);
+            const output = fs.createWriteStream(destination as string)
+            console.log('__dirname', __dirname);
+            
+            const archive = archiver('zip')
+            output.on('close', function () {
+                console.log('archiver done')
+            })
+
+            archive.on('error', function (err) {
+                throw err
+            })
+
+            archive.pipe(output)
+            console.log('source', source);
+
+
+            archive.glob('**/*', {
+                cwd: source,
+            })
+            archive.finalize()
+        }
     }
 }
